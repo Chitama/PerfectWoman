c88a01cfbc7c57a251a6fc3f0b3b5152f49d194f
Chitama <m.chuman.azalea.05@gmail.com>
2016-07-20 03:12:41 +0900

    add result and from

--- mysite/portal/urls.py
+++ mysite/portal/urls.py
@@ -1,7 +1,11 @@
+
 from django.conf.urls import url
 
 from . import views
 
+app_name = 'portal'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^result/$', views.result, name='result'),
+    url(r'^form/$', views.form, name='form'),
 ]
--- mysite/portal/views.py
+++ mysite/portal/views.py
@@ -1,7 +1,9 @@
+from django.http import Http404
 from django.http import HttpResponse
 from django.template import loader
+from django.shortcuts import render
 
-from .models import Question, Product, Category
+from .models import Product, Category
 
 
 def index(request):
@@ -11,3 +13,37 @@ def index(request):
         'category_list': category_list,
     }
     return HttpResponse(template.render(context, request))
+
+
+def result(request):
+    try:
+        product = ""
+
+        category_list = Category.objects.order_by('-category_type')[:5]
+        template = loader.get_template('portal/result.html')
+        context = {
+            'category_list': category_list,
+        }
+
+    except Product.DoesNotExist:
+        raise Http404("Product does not exist")
+
+    return HttpResponse(template.render(context, request))
+    #return render(request, 'portal/result.html', {'product': product})
+
+
+def form(request):
+    try:
+        product = ""
+
+        category_list = Category.objects.order_by('-category_type')[:5]
+        template = loader.get_template('portal/form.html')
+        context = {
+            'category_list': category_list,
+        }
+
+    except Product.DoesNotExist:
+        raise Http404("Product does not exist")
+
+    return HttpResponse(template.render(context, request))
+    #return render(request, 'portal/form.html', {'product': product})
